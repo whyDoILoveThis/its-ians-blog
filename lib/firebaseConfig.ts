2ef135ffeed391fa258379b04b2626317d9bce0a
whyDoILoveThis <136662855+whyDoILoveThis@users.noreply.github.com>
2024-07-05 15:47:18 -0500

    install dep and implement clerk

---
+++ lib/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDX3RL8BysOy-Kw_JXj0p2Z5ZVvs-THQSA",
+  authDomain: "its-ians-blog.firebaseapp.com",
+  projectId: "its-ians-blog",
+  storageBucket: "its-ians-blog.appspot.com",
+  messagingSenderId: "44923660317",
+  appId: "1:44923660317:web:da4d1caa4edfadd703fef4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
